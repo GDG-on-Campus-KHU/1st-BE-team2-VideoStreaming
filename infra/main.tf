0206225ca95085c6d8dc9f7c3377a55269fa47ca
Your Name <kjeng7897@gmail.com>
2024-12-03 15:11:34 +0900

    comment:주석 추가

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,12 @@
 # Google Cloud Provider 설정
 provider "google" {
   credentials = file("key.json")  # GCP 서비스 계정 키 파일 경로
-  project     = "gdg-side-project" # GCP 프로젝트 ID
-  region      = "us-central1"     # 원하는 리전
+  project     = "var.project_id" # GCP 프로젝트 ID
+  region      = "var.region"     # 원하는 리전
 }
+
+# Google Client Config
+data "google_client_config" "default" {}
 
 # Kubernetes Provider 설정
 provider "kubernetes" {
@@ -15,15 +18,16 @@ provider "kubernetes" {
 # GKE 클러스터 선언
 resource "google_container_cluster" "gdg_cluster" {
   name     = "gdg-cluster"
-  location = "us-central1"
+  location = "var.region"
+}
 
-  initial_node_count = 2
+
+resource "google_container_node_pool" "default_pool" {
+  cluster = google_container_cluster.gdg_cluster.name
+  name    = "gdg-node-pool"
 
   node_config {
     machine_type = "e2-medium"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
-    ]
   }
 }
 
@@ -34,5 +38,4 @@ resource "kubernetes_namespace" "monitoring" {
   }
 }
 
-# Google Client Config
-data "google_client_config" "default" {}
+
